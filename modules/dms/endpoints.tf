1668ae55b5e1dc0bc67b5a249e53f02b8f566955
Wesley Bortolozo <wesley.bortolozo@inflection.com>
2021-08-21 16:36:50 -0300

    testing extra connection on s3 target endpoint

--- modules/dms/endpoints.tf
+++ modules/dms/endpoints.tf
@@ -19,14 +19,14 @@ resource "aws_dms_endpoint" "target_s3_raw_bucket" {
   endpoint_id                 = "${var.environment}-s3-raw-target"
   endpoint_type               = "target"
   engine_name                 = "s3"
-  extra_connection_attributes = "dataFormat=parquet;addColumnName=true;timestampColumnName=dms_load_timestamp;cdcMinFileSize=100;cdcMaxBatchInterval=180;cdcInsertsAndUpdates=true;includeOpForFullLoad=True"
+  extra_connection_attributes = "bucketFolder=postgres-db;dataFormat=parquet;addColumnName=true;timestampColumnName=dms_load_timestamp;cdcMinFileSize=100;cdcMaxBatchInterval=180;cdcInsertsAndUpdates=true;includeOpForFullLoad=True"
   s3_settings {
-    bucket_folder           = "postgres-db"
+    # bucket_folder           = "postgres-db"
     bucket_name             = var.raw_bucket_name
     service_access_role_arn = aws_iam_role.dms_s3_role.arn
   }
 
   tags = {
-    dbInstanceIdentifier = jsondecode(var.source_secrets_string)["dbInstanceIdentifier"]
+    targetBucket = var.raw_bucket_name
   }
 }
